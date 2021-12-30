8eef538359fea4166fa9c05701ad3e1dc90920ee
Tony Cui <tony.tcui@gmail.com>
2021-12-30 23:16:17 +1100

    add deploy user functions entry

--- generate_website.py
+++ generate_website.py
@@ -157,6 +157,7 @@ for lang in ['en', 'zh-CN']:
     mfpcompilingFunctionsName = 'MFP compiling' if lang != 'zh-CN' else 'MFP编译函数'
     othersFunctionsName = 'others' if lang != 'zh-CN' else '其它函数'
 
+    deployUserFunctionsTopicName = 'deploy user functions' if lang != 'zh-CN' else '部署用户自定义函数'
     useMFPAnLibTopicName = 'call MFP in your app' if lang != 'zh-CN' else '在您的应用中调用MFP'
     buildAPKTopicName = 'build Android APK' if lang != 'zh-CN' else '创建安卓安装包'
 
@@ -181,6 +182,8 @@ for lang in ['en', 'zh-CN']:
     allNodes.add(languageNode)
     functionIndexNode = MFPWebSiteNode(functionIndexName, 'FunctionInfo/functions.html', websiteRootNode)
     allNodes.add(functionIndexNode)
+    deployUserFunctionsNode = MFPWebSiteNode(deployUserFunctionsTopicName, 'HowtoInfo/deploy_user_functions.html', websiteRootNode)
+    allNodes.add(deployUserFunctionsNode)
     useMFPAnLibTopicNode = MFPWebSiteNode(useMFPAnLibTopicName, 'HowtoInfo/use_mfp_android_lib.html', websiteRootNode)
     allNodes.add(useMFPAnLibTopicNode)
     buildAPKTopicNode = MFPWebSiteNode(buildAPKTopicName, 'HowtoInfo/build_apk.html', websiteRootNode)
